4d4dd8ee85b68fc3ef2684a8e43cc31cb8ed8d17
Bitzbytz <100247721+Bitzbytz@users.noreply.github.com>
2023-08-08 13:40:16 -0400

    Looping of background

--- Netizen/Assets/Scripts/CameraController.cs
+++ Netizen/Assets/Scripts/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player1;
     public GameObject player2;
+    public float minX, maxX, minY, maxY;
 
     private Vector3 target;
     private float smooth = 0.3f;
@@ -20,6 +21,6 @@ public class CameraController : MonoBehaviour
     {
         target = (player1.transform.position + player2.transform.position) / 2;
         transform.position = Vector3.Lerp(transform.position, target, smooth);
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -20, 20),  Mathf.Clamp(transform.position.y, 0, 10), -10);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX),  Mathf.Clamp(transform.position.y, minY, maxY), -10);
     }
 }
